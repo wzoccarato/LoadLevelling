be658ef2e60f1371c042ff9c815d5aa4ccfa0ec4
Walter Zoccarato <walter.zoccarato@siplus.it>
2017-09-19 16:24:36 +0200

    Creato progetto Database SDG_COnsulting

---
+++ CalcExtendedLogics/CalcExLogicClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcExtendedLogics
+{
+    public class CalcExLogicClass
+    {
+        public static bool Execute(DataSet dataset, string requiredoperation, string targetdatatablename)
+        {
+            try
+            {
+                switch (requiredoperation)
+                {
+                    case "goz":
+                        var targetdt = dataset.Tables[targetdatatablename];
+                        break;
+                    default:
+                        break;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+    }
+}
