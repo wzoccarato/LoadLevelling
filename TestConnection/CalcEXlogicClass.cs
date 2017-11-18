14b1e28483b677eb0800d0526882a8e5c93af78d
wzoccarato <Qw1357?Po>
2017-11-18 18:24:21 +0100

    *

---
+++ TestConnection/CalcEXlogicClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcExtendedLogics
+{
+    public class CalcEXlogicClass
+    {
+        #region metodi pubblici
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        /// <param name="dataset"></param>
+        /// <param name="requiredoperation"></param>
+        /// <param name="targetdatatablename"></param>
+        /// <returns></returns>
+        public static bool Execute(DataSet dataset, string requiredoperation, string targetdatatablename)
+        {
+            return true;
+        }
+        #endregion
+    }
+}
